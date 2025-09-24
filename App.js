29bc5dd44d595ef4f9970cd375660efe52ea28f5
Vallecera_FM/github_username <franzynvall@gmail.com>
2025-09-24 22:58:34 +0800

    Update App.js

--- App.js
+++ App.js
@@ -1,12 +1,10 @@
 import React from 'react';
 import { SafeAreaView } from 'react-native';
-import MidtermAct01 from './MidtermAct01';
 import CounterApp from './CounterApp';
 import ColorChangerApp from './ColorChangerApp';
 export default function App() {
   return (
     <SafeAreaView style={{ flex: 1 }}>
-     <MidtermAct01/>
        <CounterApp/>
     <ColorChangerApp/>
     </SafeAreaView>
